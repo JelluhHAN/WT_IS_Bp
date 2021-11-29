c36cc68b37b2503d9f1c1361cc4919f55fff03f3
JelluhHAN <j.stoetman@student.han.nl>
2021-11-29 12:54:54 +0100

    Update index.php

--- webserver/applicatie/src/views/index.php
+++ webserver/applicatie/src/views/index.php
@@ -1,7 +1,18 @@
 <?php
-
-declare(strict_types=1);
-
-var_dump(PDO::getAvailableDrivers());
-
-phpinfo();
+  // Hier kan ook PHP-broncode staan.
+?>
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kennismaking met PHP - voorbeeld 1</title>
+  </head>
+  <body>
+    <h1>Kennismaking met PHP - voorbeeld 1</h1>
+    <p>
+      <?php echo "De eerste PHP-pagina van de cursus WTIS aan de HAN. "?>
+    </p>
+    <p><strong>Gebruik de ‘DevTools’ om vast te stellen of alle PHP-broncode omgezet is naar HTML!</strong></p>
+  </body>
+</html>
